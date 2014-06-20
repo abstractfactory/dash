ca814926cd9d6189aedd0dd2ff4f4f8c9bf486c6
Marcus Ottosson <konstruktion@gmail.com>
2014-06-20 15:17:25 +0100

    Wrap-up

--- dash/main.pyw
+++ dash/main.pyw
@@ -2,47 +2,19 @@
 
 """Main entry point for build"""
 
-import os
-import sys
-
-
-def add_to_path():
-    root = os.path.abspath(__file__)
-    for x in range(3):
-        root = os.path.dirname(root)
-    sys.path.insert(0, root)
-    print "I: adding %r to PYTHONPATH" % root
-
-
-def check_dependencies():
-    import pifou
-    if pifou.missing_dependencies:
-        import pifou.error
-        raise pifou.error.Dependency(pifou.missing_dependencies)
-
-
-def get_path():
-    try:
-        path = sys.argv[1]
-        print "I: Using ARGS"
-    except:
-        path = os.getcwd()
-        print "I: Using CWD"
-    return path
-
-
+# standard library
 import logging
+import argparse
+
+# local library
+import dash.version
+import dash.presentation
+
 log = logging.getLogger('dash')
 log.setLevel(logging.WARNING)
 
-if __name__ == '__main__':
-    check_dependencies()
-    add_to_path()
-
-    import dash.version
-
-    message = '''
- ____________
+message = '''
+____________
 |            |
 | Dash {} |
 |____________|
@@ -51,10 +23,15 @@ Press CTRL-C to quit..
 
 -----------------------
 '''.format(dash.version.version)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('path', help="Absolute path to root directory")
+
+    args = parser.parse_args()
+
     print message
+    print "I: running Dash @ %r" % args.path
 
-    import dash.presentation
-    path = get_path()
-
-    print "I: running Dash @ %r" % path
-    dash.presentation.main(path)
+    dash.presentation.main(path=args.path)
--- dash/presentation.py
+++ dash/presentation.py
@@ -1,77 +1,36 @@
 
 from __future__ import absolute_import
 
-# pifou library
+# pipi library
 import pifou
-import pifou.com.error
-
-# pigui library
 import pigui
 
 # local library
-import dash.widget
+import dash.controller
 import dash.application
 
-# pifou dependency
-import zmq
-
-# Loggers
 pifou.setup_log()
 pigui.setup_log()
-
-
-def get_root():
-    root = pifou.pi.ROOT
-
-    if not root:
-        raise pifou.error.Root
-
-    return root
-
-
-def request_application():
-    """Look for existing instance of Dashboard"""
-    print "I: Requesting a new Widget"
-
-    # Try default address
-    context = zmq.Context.instance()
-    query = context.socket(zmq.REQ)
-    query.connect("tcp://127.0.0.1:5555")
-    query.send_json({'restore': True})
-
-    poll = zmq.Poller()
-    poll.register(query, zmq.POLLIN)
-
-    sockets = poll.poll(timeout=200)
-    if query in dict(sockets):
-        message = query.recv_json()
-        if message.get('status') == 'ok':
-            return True
-    else:
-        poll.unregister(query)
-        query.close()
-        return False
 
 
 def main(path):
     import pigui.pyqt5.util
 
-    if not request_application():
-        with pigui.pyqt5.util.application_context():
-            win = dash.widget.Dash()
+    with pigui.pyqt5.util.application_context():
+        win = dash.controller.Dash()
+        app = dash.application.Dash()
 
-            model = dash.model.Model()
+        model = dash.model.Model()
 
-            app = dash.application.Dash()
-            app.set_widget(win)
-            app.set_model(model)
+        app.set_controller(win)
+        app.set_model(model)
 
-            model.setup(uri='disk:' + path)
+        model.setup(path)
 
-            win.resize(*dash.settings.WINDOW_SIZE)
-            win.show()
+        win.resize(*dash.settings.WINDOW_SIZE)
+        win.animated_show()
 
 
 if __name__ == '__main__':
-    path = get_root()
-    main(path)
+    import os
+    main(os.path.expanduser('~'))
